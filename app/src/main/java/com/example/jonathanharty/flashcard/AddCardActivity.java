25e5f087b5a6a172ccf8ad6c42927b2fd7242e49
jonathan-harty <jonathanharty99@gmail.com>
2018-11-16 23:58:44 -0600

    added connection to database

--- app/src/main/java/com/example/jonathanharty/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/jonathanharty/flashcard/AddCardActivity.java
@@ -3,8 +3,10 @@ package com.example.jonathanharty.flashcard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -12,6 +14,30 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        String currentQuestion = getIntent().getStringExtra("currentQuestion");
+        String currentAnswer = getIntent().getStringExtra("currentAnswer");
+
+        String answerChoice1 = getIntent().getStringExtra("answerChoice1");
+        String answerChoice2 = getIntent().getStringExtra("answerChoice2");
+        String answerChoice3 = getIntent().getStringExtra("answerChoice3");
+
+        if(answerChoice1 != null && answerChoice1.equals(currentAnswer)) {
+            ((EditText)findViewById(R.id.edit_incorrect_1)).setText(answerChoice2);
+            ((EditText)findViewById(R.id.edit_incorrect_2)).setText(answerChoice3);
+        }
+        else if(answerChoice2 != null && answerChoice2.equals(currentAnswer)) {
+            ((EditText)findViewById(R.id.edit_incorrect_1)).setText(answerChoice1);
+            ((EditText)findViewById(R.id.edit_incorrect_2)).setText(answerChoice3);
+        }
+        else if(answerChoice3 != null && answerChoice3.equals(currentAnswer)) {
+            ((EditText)findViewById(R.id.edit_incorrect_1)).setText(answerChoice1);
+            ((EditText)findViewById(R.id.edit_incorrect_2)).setText(answerChoice2);
+        }
+
+        ((EditText)findViewById(R.id.edit_question)).setText(currentQuestion);
+        ((EditText)findViewById(R.id.edit_answer)).setText(currentAnswer);
+
 
         findViewById(R.id.cancel_add_card).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,12 +52,25 @@ public class AddCardActivity extends AppCompatActivity {
                 String questionText = ((EditText)findViewById(R.id.edit_question)).getText().toString();
                 String answerText = ((EditText)findViewById(R.id.edit_answer)).getText().toString();
 
-                Intent data = new Intent();
-                data.putExtra("question", questionText);
-                data.putExtra("answer", answerText);
+                String incorrect1Text = ((EditText)findViewById(R.id.edit_incorrect_1)).getText().toString();
+                String incorrect2Text = ((EditText)findViewById(R.id.edit_incorrect_2)).getText().toString();
 
-                setResult(100, data);
-                finish();
+                if(questionText.equals("") || answerText.equals("") || incorrect1Text.equals("") || incorrect2Text.equals("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Question and Answer Needed", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                }
+                else {
+                    Intent data = new Intent();
+                    data.putExtra("question", questionText);
+                    data.putExtra("answer", answerText);
+
+                    data.putExtra("incorrect1", incorrect1Text);
+                    data.putExtra("incorrect2", incorrect2Text);
+
+                    setResult(100, data);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/jonathanharty/flashcard/MainActivity.java
+++ app/src/main/java/com/example/jonathanharty/flashcard/MainActivity.java
@@ -1,27 +1,72 @@
 package com.example.jonathanharty.flashcard;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amitshekhar.DebugDB;
+
+import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
     boolean isShowingAnswers = false;
+    boolean isShowingQuestionSide = true;
+    boolean emptyState = true;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
+
+    FlashcardDatabase flashcardDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if(allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView)findViewById(R.id.flashcardQuestion)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView)findViewById(R.id.flashcardAnswer)).setText(allFlashcards.get(0).getAnswer());
+
+            int correctPosition = getRandNum(1, 3);
+
+            if(correctPosition == 1) {
+                ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(0).getAnswer());
+                ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(0).getWrongAnswer1());
+                ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(0).getWrongAnswer2());
+            }
+            else if(correctPosition == 2) {
+                ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(0).getWrongAnswer1());
+                ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(0).getAnswer());
+                ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(0).getWrongAnswer2());
+            }
+            else {
+                ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(0).getWrongAnswer1());
+                ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(0).getWrongAnswer2());
+                ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(0).getAnswer());
+            }
+        }
+        else {
+            displayEmpty();
+            emptyState = true;
+        }
+
         findViewById(R.id.flashcardQuestion).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcardQuestion).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcardAnswer).setVisibility(View.VISIBLE);
+
+                isShowingQuestionSide = false;
             }
         });
 
@@ -30,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 findViewById(R.id.flashcardQuestion).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcardAnswer).setVisibility(View.INVISIBLE);
+
+                isShowingQuestionSide = true;
             }
         });
 
@@ -44,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
                     findViewById(R.id.choice1).setVisibility(View.INVISIBLE);
                     findViewById(R.id.choice2).setVisibility(View.INVISIBLE);
                     findViewById(R.id.choice3).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.choice4).setVisibility(View.INVISIBLE);
                     ((ImageView)findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.view_answers_symbol);
                 }
                 else {
@@ -53,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
                     findViewById(R.id.choice1).setVisibility(View.VISIBLE);
                     findViewById(R.id.choice2).setVisibility(View.VISIBLE);
                     findViewById(R.id.choice3).setVisibility(View.VISIBLE);
-                    findViewById(R.id.choice4).setVisibility(View.VISIBLE);
                     ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.hide_answers_symbol);
                 }
             }
@@ -66,16 +111,206 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+
+                intent.putExtra("currentQuestion", ((TextView)findViewById(R.id.flashcardQuestion)).getText());
+                intent.putExtra("currentAnswer", ((TextView)findViewById(R.id.flashcardAnswer)).getText());
+
+                intent.putExtra("answerChoice1", ((TextView)findViewById(R.id.choice1)).getText());
+                intent.putExtra("answerChoice2", ((TextView)findViewById(R.id.choice2)).getText());
+                intent.putExtra("answerChoice3", ((TextView)findViewById(R.id.choice3)).getText());
+
+
+                MainActivity.this.startActivityForResult(intent, 110);
+            }
+        });
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(allFlashcards.size() > 0) {
+                    if(!isShowingQuestionSide) {
+                        findViewById(R.id.flashcardQuestion).setVisibility(View.VISIBLE);
+                        findViewById(R.id.flashcardAnswer).setVisibility(View.INVISIBLE);
+                    }
+
+                    currentCardDisplayedIndex = getRandNum(0, allFlashcards.size() - 1);
+
+                    if(currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                        currentCardDisplayedIndex = 0;
+                    }
+
+                    displayNext();
+                    emptyState = false;
+                }
+            }
+        });
+
+        findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(allFlashcards.size() > 0) {
+                    flashcardDatabase.deleteCard(((TextView)findViewById(R.id.flashcardQuestion)).getText().toString());
+                    allFlashcards.remove(currentCardDisplayedIndex);
+
+                    if(allFlashcards.size() > 0) {
+                        currentCardDisplayedIndex = getRandNum(0, allFlashcards.size() - 1);
+
+                        displayNext();
+                        emptyState = false;
+                    }
+                    else {
+                        emptyState = true;
+                        displayEmpty();
+                    }
+
+                }
+                else {
+                    emptyState = true;
+                    displayEmpty();
+                }
+
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 100) {
+        if(requestCode == 100 && resultCode == 100) {
             String question = data.getExtras().getString("question");
             String answer = data.getExtras().getString("answer");
 
+            String incorrect_1 = data.getExtras().getString("incorrect1");
+            String incorrect_2 = data.getExtras().getString("incorrect2");
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer, incorrect_1, incorrect_2));
+            allFlashcards = flashcardDatabase.getAllCards();
+
             ((TextView)findViewById(R.id.flashcardQuestion)).setText(question);
             ((TextView)findViewById(R.id.flashcardAnswer)).setText(answer);
+
+            int correctPosition = getRandNum(1, 3);
+
+            if(correctPosition == 1) {
+                ((TextView)findViewById(R.id.choice1)).setText(answer);
+                ((TextView)findViewById(R.id.choice2)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice3)).setText(incorrect_2);
+            }
+            else if(correctPosition == 2) {
+                ((TextView)findViewById(R.id.choice1)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice2)).setText(answer);
+                ((TextView)findViewById(R.id.choice3)).setText(incorrect_2);
+            }
+            else {
+                ((TextView)findViewById(R.id.choice1)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice2)).setText(incorrect_2);
+                ((TextView)findViewById(R.id.choice3)).setText(answer);
+            }
+
+            Snackbar.make(findViewById(R.id.flashcardQuestion), "Question Added", Snackbar.LENGTH_SHORT).show();
+
+            if(emptyState) {
+                findViewById(R.id.empty).setVisibility(View.INVISIBLE);
+                findViewById(R.id.toggle_choices_visibility).setVisibility(View.VISIBLE);
+                if(isShowingAnswers) {
+                    findViewById(R.id.choice1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.choice2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.choice3).setVisibility(View.VISIBLE);
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.hide_answers_symbol);
+                }
+                else {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.view_answers_symbol);
+                }
+                findViewById(R.id.flashcardQuestion).setVisibility(View.VISIBLE);
+                findViewById(R.id.edit_button).setVisibility(View.VISIBLE);
+                findViewById(R.id.next_button).setVisibility(View.VISIBLE);
+                findViewById(R.id.delete_button).setVisibility(View.VISIBLE);
+                displayNext();
+            }
+        }
+        else if(requestCode == 110 && resultCode == 100) {
+
+            String question = data.getExtras().getString("question");
+            String answer = data.getExtras().getString("answer");
+
+            String incorrect_1 = data.getExtras().getString("incorrect1");
+            String incorrect_2 = data.getExtras().getString("incorrect2");
+
+            ((TextView)findViewById(R.id.flashcardQuestion)).setText(question);
+            ((TextView)findViewById(R.id.flashcardAnswer)).setText(answer);
+
+            int correctPosition = getRandNum(1, 3);
+
+            if(correctPosition == 1) {
+                ((TextView)findViewById(R.id.choice1)).setText(answer);
+                ((TextView)findViewById(R.id.choice2)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice3)).setText(incorrect_2);
+            }
+            else if(correctPosition == 2) {
+                ((TextView)findViewById(R.id.choice1)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice2)).setText(answer);
+                ((TextView)findViewById(R.id.choice3)).setText(incorrect_2);
+            }
+            else {
+                ((TextView)findViewById(R.id.choice1)).setText(incorrect_1);
+                ((TextView)findViewById(R.id.choice2)).setText(incorrect_2);
+                ((TextView)findViewById(R.id.choice3)).setText(answer);
+            }
+
+            Flashcard curr = allFlashcards.get(currentCardDisplayedIndex);
+            curr.setQuestion(question);
+            curr.setAnswer(answer);
+            curr.setWrongAnswer1(incorrect_1);
+            curr.setWrongAnswer2(incorrect_2);
+
+            flashcardDatabase.updateCard(curr);
+
+            Snackbar.make(findViewById(R.id.flashcardQuestion), "Question Updated", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    public int getRandNum(int minNum, int maxNum) {
+        Random rand = new Random();
+        return rand.nextInt((maxNum - minNum) + 1) + minNum;
+    }
+
+    public void displayEmpty() {
+        findViewById(R.id.empty).setVisibility(View.VISIBLE);
+        findViewById(R.id.choice1).setVisibility(View.INVISIBLE);
+        findViewById(R.id.choice2).setVisibility(View.INVISIBLE);
+        findViewById(R.id.choice3).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcardQuestion).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcardAnswer).setVisibility(View.INVISIBLE);
+        findViewById(R.id.edit_button).setVisibility(View.INVISIBLE);
+        findViewById(R.id.toggle_choices_visibility).setVisibility(View.INVISIBLE);
+        findViewById(R.id.next_button).setVisibility(View.INVISIBLE);
+        findViewById(R.id.delete_button).setVisibility(View.INVISIBLE);
+    }
+
+    public void displayNext() {
+        ((TextView)findViewById(R.id.flashcardQuestion)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+        ((TextView)findViewById(R.id.flashcardAnswer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+        int correctPosition = getRandNum(1, 3);
+
+        if(correctPosition == 1) {
+            ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+            ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+        }
+        else if(correctPosition == 2) {
+            ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+            ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+        }
+        else {
+            ((TextView)findViewById(R.id.choice1)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+            ((TextView)findViewById(R.id.choice2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+            ((TextView)findViewById(R.id.choice3)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
         }
     }
 }
